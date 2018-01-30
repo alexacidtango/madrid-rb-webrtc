5e4da2e9880876d9e01f6582f0e257e4a41121ef
Alejandro González Núñez <agonzaleznu@gmail.com>
2018-01-30 15:27:57 +0100

    Jquery and channel

---
+++ app/assets/javascripts/webrtc.js
@@ -0,0 +1,15 @@
+window.onload() = function() {
+  initialize();
+};
+
+function initialize() {
+  console.log("initializing");
+};
+
+function joinSession() {
+  console.log("Join Session");
+};
+
+function leaveSession() {
+  console.log("Leave Session");
+};
